140ec6459216d2753599e25a17a28a293dbdcedf
Michael Gencarelli <mgencarelli@hitachisolutions.com>
2022-04-23 16:02:08 -0400

    Add project files.

---
+++ MyShoppingCart/Components/ItemComponent.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MyShoppingCart.Components
+{
+    public partial class ItemComponent : ComponentBase
+    {
+        [Parameter]
+        public string Name { get; set; } = "";
+
+        [Parameter]
+        public int Count { get; set; } = 0;
+    }
+}
---
+++ MyShoppingCart/Pages/ShoppingCart.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MyShoppingCart.Pages
+{
+    public partial class ShoppingCart : ComponentBase
+    {
+        private int NumApples { get; set; } = 0;
+        private int NumBananas { get; set; } = 0;
+        private int NumOranges { get; set; } = 0;
+
+        private void AddApple()
+        {
+            NumApples++;
+        }
+        private void AddBanana()
+        {
+            NumBananas++;
+        }
+        private void AddOrange()
+        {
+            NumOranges++;
+        }
+    }
+}
